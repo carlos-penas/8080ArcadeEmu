6a6580c05af98611c62f192ab63a9cd9135e468f
Carlos Peñas <carlos.penas.n@gmail.com>
2021-09-05 11:08:12 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char** argv) {
+
+    cout << "hola";
+    return 0;
+}
+
